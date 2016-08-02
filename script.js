ac843226e73322db300ebcffda51628a20c9c08d
philippissas <philip@pissas.com>
2016-08-02 17:48:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('Oh! Hi there. I'm just a bot. Say HELLO to get started.')
+            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
                 .then(() => 'speak');
         }
     },
