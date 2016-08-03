70a634b04fcbc38a19a2ba4e102211a2983dae09
philippissas <philip@pissas.com>
2016-08-03 02:24:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
         start: {
         receive: (bot) => {
-            return bot.say('Oh, hello! Sorry, I\'m just a bot.)
+            return bot.say('Oh, hello! Sorry.)
                 .then(() => 'askName');
         }
     },
